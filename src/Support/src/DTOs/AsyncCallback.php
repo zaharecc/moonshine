c089941a1c3bb4b57b6f2937832f0fc35643763a
levchenko-ivan <ivan97111@yandex.ru>
2024-10-26 13:14:25 +0300

    fix: Fragment async

--- src/Support/src/DTOs/AsyncCallback.php
+++ src/Support/src/DTOs/AsyncCallback.php
@@ -5,35 +5,62 @@ declare(strict_types=1);
 namespace MoonShine\Support\DTOs;
 
 use Illuminate\Contracts\Support\Arrayable;
+use JsonSerializable;
 
-final readonly class AsyncCallback implements Arrayable
+final readonly class AsyncCallback implements Arrayable, JsonSerializable
 {
     public function __construct(
-        private ?string $success,
-        private ?string $before,
+        /**
+         * Replacing the async request response handler
+         */
+        private ?string $responseCallback,
+
+        /**
+         * Will be called before the async request
+         */
+        private ?string $beforeResponse,
+
+        /**
+         * Will be called after native request processing if $responseCallback is not specified
+         */
+        private ?string $afterCallback,
     ) {
     }
 
-    public static function with(?string $success = null, ?string $before = null): self
-    {
-        return new self($success, $before);
+    public static function with(
+        ?string $responseCallback = null,
+        ?string $beforeResponse = null,
+        ?string $afterCallback = null
+    ): self {
+        return new self($responseCallback, $beforeResponse, $afterCallback);
     }
 
-    public function getBefore(): ?string
+    public function responseCallback(): ?string
     {
-        return $this->before;
+        return $this->responseCallback;
     }
 
-    public function getSuccess(): ?string
+    public function beforeResponse(): ?string
     {
-        return $this->success;
+        return $this->beforeResponse;
+    }
+
+    public function afterCallback(): ?string
+    {
+        return $this->afterCallback;
     }
 
     public function toArray(): array
     {
         return [
-            'before' => $this->getBefore(),
-            'success' => $this->getSuccess(),
+            'responseCallback' => $this->responseCallback(),
+            'beforeResponse' => $this->beforeResponse(),
+            'afterCallback' => $this->afterCallback(),
         ];
+    }
+
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
     }
 }
