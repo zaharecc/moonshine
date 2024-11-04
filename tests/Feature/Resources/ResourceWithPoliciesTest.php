a1ba158403498a7c6ad60917d2c80a1cef493547
levchenko-ivan <ivan97111@yandex.ru>
2024-11-04 14:11:18 +0300

    fix: policies now works correctly

---
+++ tests/Feature/Resources/ResourceWithPoliciesTest.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+use MoonShine\Contracts\UI\FormBuilderContract;
+use MoonShine\Laravel\Models\MoonshineUser;
+use MoonShine\Laravel\Pages\Crud\DetailPage;
+use MoonShine\Laravel\Pages\Crud\IndexPage;
+use MoonShine\Support\Enums\HttpMethod;
+use MoonShine\Tests\Fixtures\Models\Item;
+use MoonShine\Tests\Fixtures\Resources\TestResourceBuilder;
+use MoonShine\UI\Components\ActionButton;
+use MoonShine\UI\Fields\ID;
+use MoonShine\UI\Fields\Text;
+
+uses()->group('resources-feature');
+uses()->group('resources-policies');
+
+beforeEach(function (): void {
+
+    $this->item = createItem(1, 1);
+
+    $this->resource = TestResourceBuilder::new(Item::class)
+        ->setTestFields([
+            ID::make()->sortable(),
+            Text::make('Name', 'name')->sortable(),
+        ])
+        ->setTestPolicy(true)
+    ;
+});
+
+it('policies in index', function () {
+    expect($this->resource->isWithPolicy())
+        ->toBeTrue();
+
+    $createButton = ActionButton::make(
+        __('moonshine::ui.create'),
+        $this->resource->getFormPageUrl()
+    )
+        ->name('resource-create-button')
+        ->primary()
+        ->icon('plus');
+
+    $resource = $this->resource;
+
+    $massButton = ActionButton::make(
+        '',
+        url: static fn (): string => $resource->getRoute('crud.massDelete')
+    )
+        ->name('mass-delete-button')
+        ->bulk($this->resource->getListComponentName())
+        ->withConfirm(
+            method: HttpMethod::DELETE,
+            formBuilder: static fn (FormBuilderContract $formBuilder): FormBuilderContract => $formBuilder->when(
+                $resource->isAsync(),
+                static fn (FormBuilderContract $form): FormBuilderContract => $form->async(
+                    events: $resource->getListEventName(
+                        $resource->getListComponentName()
+                    )
+                )
+            ),
+            name: 'resource-mass-delete-modal'
+        )
+        ->error()
+        ->icon('trash')
+        ->showInLine();
+
+    asAdmin()->get(
+        $this->moonshineCore->getRouter()->getEndpoints()
+            ->toPage(page: IndexPage::class, resource: $this->resource)
+    )
+        ->assertSeeHtml($createButton)
+        ->assertSeeHtml($massButton)
+        ->assertOk();
+
+});
+
+it('policies index forbidden', function () {
+    MoonshineUser::query()->where('id', 1)->update([
+        'name' => 'Policies test'
+    ]);
+
+    asAdmin()->get(
+        $this->moonshineCore->getRouter()->getEndpoints()
+            ->toPage(page: IndexPage::class, resource: $this->resource)
+    )
+        ->assertForbidden();
+});
+
+it('policies in detail', function () {
+    asAdmin()->get(
+        $this->moonshineCore->getRouter()->getEndpoints()
+            ->toPage(page: DetailPage::class, resource: $this->resource, params: ['resourceItem' => $this->item->id])
+    )
+        ->assertForbidden()
+    ;
+});
---
+++ tests/Fixtures/Policies/ItemPolicy.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MoonShine\Tests\Fixtures\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use MoonShine\Laravel\Models\MoonshineUser;
+use MoonShine\Tests\Fixtures\Models\Item;
+
+class ItemPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(MoonshineUser $user)
+    {
+        if($user->name === 'Policies test') {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function view(MoonshineUser $user, Item $item)
+    {
+        return false;
+    }
+
+    public function create(MoonshineUser $user)
+    {
+        return true;
+    }
+
+    public function update(MoonshineUser $user, Item $item)
+    {
+        return true;
+    }
+
+    public function delete(MoonshineUser $user, Item $item)
+    {
+        return $user->moonshine_user_role_id === 1;
+    }
+
+    public function restore(MoonshineUser $user, Item $item)
+    {
+        return $user->moonshine_user_role_id === 1;
+    }
+
+    public function forceDelete(MoonshineUser $user, Item $item)
+    {
+        return $user->moonshine_user_role_id === 1;
+    }
+
+    public function massDelete(MoonshineUser $user)
+    {
+        return $user->moonshine_user_role_id === 1;
+    }
+}
