9e844adbddc4860b71937ba6a0665ef8bac94391
second1996 <vital10012@gmail.com>
2024-11-17 20:34:18 +0200

    fix: Layout menu CSS

--- src/UI/resources/js/Components/NavTooltip.js
+++ src/UI/resources/js/Components/NavTooltip.js
@@ -2,7 +2,13 @@ import tippy from 'tippy.js'
 
 export default () => ({
   tooltipInstance: null,
+  horizontalMenuEl: document.querySelector('.layout-menu-horizontal'),
+
   init() {
+    if (this.horizontalMenuEl && this.horizontalMenuEl.contains(this.$el)) {
+      return
+    }
+
     this.tooltipInstance = tippy(this.$el, {
       placement: 'auto',
       offset: [0, 30],
@@ -10,7 +16,12 @@ export default () => ({
       content: () => this.$el.querySelector('.menu-inner-text').textContent,
     })
   },
+
   toggleTooltip() {
+    if (this.horizontalMenuEl && this.horizontalMenuEl.contains(this.$el)) {
+      return
+    }
+
     const lgMediaQuery = window.matchMedia('(min-width: 1024px) and (max-width: 1279.98px)')
 
     if (!this.$data.minimizedMenu && !lgMediaQuery.matches) {
