abe0b2a846bcdc03b4e59a642227420025d59061
LT <leetoplayaz@yandex.ru>
2024-12-14 18:46:36 +0300

    fix: CardsBuilder with js container

--- src/UI/resources/views/components/cards.blade.php
+++ src/UI/resources/views/components/cards.blade.php
@@ -9,33 +9,34 @@
     'name' => 'default',
     'translates' => [],
 ])
-
-<div x-data="cardsBuilder(
+<div class="js-cards-builder-container">
+    <div x-data="cardsBuilder(
     {{ (int) $async }},
     '{{ $asyncUrl }}'
 )"
-    data-pushstate="{{ $attributes->get('data-pushstate', false)}}"
-    @defineEventWhen($async, 'cards_updated', $name, 'asyncRequest')
-    {{ $attributes->only(['data-events'])}}
->
-    <x-moonshine::loader x-show="loading" />
-    <div x-show="!loading">
-        @if($components->isNotEmpty())
-            <x-moonshine::layout.grid>
-                @foreach($components as $card)
-                    <x-moonshine::layout.column :colSpan="$colSpan" :adaptiveColSpan="$adaptiveColSpan">
-                        {!! $card !!}
-                    </x-moonshine::layout.column>
-                @endforeach
-            </x-moonshine::layout.grid>
+         data-pushstate="{{ $attributes->get('data-pushstate', false)}}"
+        @defineEventWhen($async, 'cards_updated', $name, 'asyncRequest')
+        {{ $attributes->only(['data-events'])}}
+    >
+        <x-moonshine::loader x-show="loading" />
+        <div x-show="!loading">
+            @if($components->isNotEmpty())
+                <x-moonshine::layout.grid>
+                    @foreach($components as $card)
+                        <x-moonshine::layout.column :colSpan="$colSpan" :adaptiveColSpan="$adaptiveColSpan">
+                            {!! $card !!}
+                        </x-moonshine::layout.column>
+                    @endforeach
+                </x-moonshine::layout.grid>
 
-            @if($hasPaginator)
-                {!! $paginator !!}
+                @if($hasPaginator)
+                    {!! $paginator !!}
+                @endif
+            @else
+                <x-moonshine::alert type="default" class="my-4" icon="s.no-symbol">
+                    {{ $translates['notfound'] }}
+                </x-moonshine::alert>
             @endif
-        @else
-            <x-moonshine::alert type="default" class="my-4" icon="s.no-symbol">
-                {{ $translates['notfound'] }}
-            </x-moonshine::alert>
-        @endif
+        </div>
     </div>
 </div>
