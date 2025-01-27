58b44a475b70e4e7652b8be59c7e22a1150b0bef
LT <leetoplayaz@yandex.ru>
2025-01-27 12:35:19 +0300

    fix: Hot
    Js events

--- src/UI/resources/js/Support/DispatchEvents.js
+++ src/UI/resources/js/Support/DispatchEvents.js
@@ -33,14 +33,7 @@ export function dispatchEvents(events, type, component, extraProperties = {}) {
       }
 
       setTimeout(function () {
-        dispatchEvent(
-          new CustomEvent(eventName.replaceAll(/\s/g, '').toLowerCase(), {
-            attributes,
-            bubbles: true,
-            composed: true,
-            cancelable: true,
-          }),
-        )
+        component.$dispatch(eventName.replaceAll(/\s/g, '').toLowerCase(), attributes)
       }, attributes['_delay'] ?? 0)
     })
   }
