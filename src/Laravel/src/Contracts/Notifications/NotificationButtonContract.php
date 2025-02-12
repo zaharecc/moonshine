30076989182e3eebd6f45c6e07f9dc7e23b9eaaa
LT <leetoplayaz@yandex.ru>
2025-02-12 17:12:30 +0300

    feat: NotificationButton with attributes

--- src/Laravel/src/Contracts/Notifications/NotificationButtonContract.php
+++ src/Laravel/src/Contracts/Notifications/NotificationButtonContract.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace MoonShine\Laravel\Contracts\Notifications;
 
+use Illuminate\Contracts\Support\Arrayable;
 use MoonShine\Contracts\UI\ComponentAttributesBagContract;
 
+/**
+ * @mixin Arrayable
+ */
 interface NotificationButtonContract
 {
     public function getLink(): string;
