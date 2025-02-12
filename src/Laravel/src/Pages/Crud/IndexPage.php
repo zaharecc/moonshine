2bc8ec80fa033387bea4f52adb2dd0df58bf20bc
LT <leetoplayaz@yandex.ru>
2025-02-12 11:01:48 +0300

    fix: all to query

--- src/Laravel/src/Pages/Crud/IndexPage.php
+++ src/Laravel/src/Pages/Crud/IndexPage.php
@@ -210,7 +210,7 @@ class IndexPage extends CrudPage
             ->clickAction($this->getResource()->getClickAction())
             ->when($this->getResource()->isAsync(), function (TableBuilderContract $table): void {
                 $table->async(
-                    url: fn() => $this->getRouter()->getEndpoints()->component(name: $table->getName(), additionally: request()->all())
+                    url: fn() => $this->getRouter()->getEndpoints()->component(name: $table->getName(), additionally: request()->query())
                 )->pushState();
             })
             ->when($this->getResource()->isStickyTable(), function (TableBuilderContract $table): void {
