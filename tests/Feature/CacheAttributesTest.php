afcf47f3cfa7758532769609764510b1c379e772
LT <leetoplayaz@yandex.ru>
2025-02-28 18:36:59 +0300

    chore: up dep

--- tests/Feature/CacheAttributesTest.php
+++ tests/Feature/CacheAttributesTest.php
@@ -4,12 +4,7 @@ declare(strict_types=1);
 
 use Leeto\FastAttributes\Attributes;
 use MoonShine\Contracts\Core\DependencyInjection\CacheAttributesContract;
-use MoonShine\Contracts\Core\DependencyInjection\OptimizerCollectionContract;
-use MoonShine\Contracts\MenuManager\MenuAutoloaderContract;
-use MoonShine\Contracts\MenuManager\MenuElementContract;
-use MoonShine\Laravel\Commands\OptimizeCommand;
 use MoonShine\Laravel\Resources\MoonShineUserResource;
-use MoonShine\MenuManager\MenuGroup;
 use MoonShine\Support\Attributes\Icon;
 
 uses()->group('cache-attributes');
@@ -28,7 +23,7 @@ it('resolve', function () {
 it('get attribute', function () {
     $attributes = app(CacheAttributesContract::class);
     $icon = $attributes->get(
-        default: fn() => Attributes::for(MoonShineUserResource::class, Icon::class)->class()->first('icon'),
+        default: fn() => Attributes::for(MoonShineUserResource::class, Icon::class)->first('icon'),
         target: MoonShineUserResource::class,
         attribute: Icon::class,
         column: [0 => 'icon']
@@ -37,6 +32,4 @@ it('get attribute', function () {
     expect($icon)
         ->toBe('users')
     ;
-
-    $this->artisan(OptimizeCommand::class);
 });
