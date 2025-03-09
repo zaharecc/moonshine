2392b92310f96d8b0c16c38d2d093b01d79195c9
Alexander Nikushkin <nikushkin@mail.ru>
2025-03-09 17:56:05 +0500

    chore(components): search form clear

--- src/UI/resources/views/components/layout/search-form.blade.php
+++ src/UI/resources/views/components/layout/search-form.blade.php
@@ -9,26 +9,36 @@
         <form action="{{ $action }}"
               x-ref="searchForm"
               class="search-form"
+              x-data="{ searchValue: '{{ $value }}' }"
         >
             <x-moonshine::form.input
-                x-data="{}"
+                x-model="searchValue"
                 x-ref="searchInput"
                 name="search"
                 @keyup.ctrl.k.window="$refs.searchInput.focus()"
                 @keyup.ctrl.period.window="$refs.searchInput.focus()"
                 type="search"
                 class="search-form-field form-input"
-                value="{{ $value }}"
                 placeholder="{{ $placeholder }}"
+                required
             />
+
+            <button
+                type="button"
+                class="search-form-clear"
+                x-show="searchValue"
+                @click="searchValue = ''; $refs.searchInput.value = ''; $refs.searchForm.submit()"
+            >
+                <x-moonshine::icon
+                    icon="x-mark"
+                />
+            </button>
 
             <button class="search-form-submit" type="submit">
                 <x-moonshine::icon
                     icon="magnifying-glass"
-                    size="6"
                 />
             </button>
         </form>
     </div>
 @endif
-
