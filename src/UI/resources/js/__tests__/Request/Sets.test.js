8ada1cdd1495a5d70eafa8f2c14c3dd78ed54e06
LT <leetoplayaz@yandex.ru>
2025-03-12 11:59:43 +0300

    test: Sets

--- src/UI/resources/js/__tests__/Request/Sets.test.js
+++ src/UI/resources/js/__tests__/Request/Sets.test.js
@@ -1,1 +1,16 @@
-test.todo('Will be green')
+import {listComponentRequest} from '../../Request/Sets.js'
+import {expect, it, jest} from '@jest/globals'
+
+it('should call preventDefault and set loading to true', () => {
+  const component = {
+    $event: {preventDefault: jest.fn(), detail: {}},
+    $el: {href: null},
+    asyncUrl: '/example',
+    loading: false,
+  }
+
+  listComponentRequest(component)
+
+  expect(component.$event.preventDefault).toHaveBeenCalled()
+  expect(component.loading).toBe(true)
+})
