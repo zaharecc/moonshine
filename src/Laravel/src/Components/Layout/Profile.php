09209ca0e95ac51901f5349128d4c661d8bea706
LT <leetoplayaz@yandex.ru>
2025-03-13 18:46:02 +0300

    fix: Issue
    Issue #1630

--- src/Laravel/src/Components/Layout/Profile.php
+++ src/Laravel/src/Components/Layout/Profile.php
@@ -149,7 +149,7 @@ final class Profile extends MoonShineComponent
             'route' => $this->route ?? toPage(
                 moonshineConfig()->getPage('profile', ProfilePage::class),
             ),
-            'logOutRoute' => $this->logOutRoute ?? moonshineRouter()->to('logout'),
+            'logOutRoute' => $this->logOutRoute ?? rescue(fn() => moonshineRouter()->to('logout'), ''),
             'avatar' => $avatar,
             'nameOfUser' => $nameOfUser,
             'username' => $username,
