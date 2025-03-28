08225ed85133b77dd10f5e386806bc3591adf694
Danil Shutsky <leetoplayaz@yandex.ru>
2025-03-28 15:59:26 +0300

    feat: Toast duration time

--- src/UI/resources/js/Components/FormBuilder.js
+++ src/UI/resources/js/Components/FormBuilder.js
@@ -254,8 +254,10 @@ export default (name = '', initData = {}, reactive = {}) => ({
   },
 
   dispatchEvents(componentEvent, exclude = null, extra = {}) {
+    const form = this.$el.tagName === 'FORM' ? this.$el : this.$el.closest('form')
+
     extra['_data'] =
-      exclude === '*' ? {} : formToJSON(prepareFormData(new FormData(this.$el), exclude))
+      exclude === '*' ? {} : formToJSON(prepareFormData(new FormData(form), exclude))
 
     de(componentEvent, '', this, extra)
   },
